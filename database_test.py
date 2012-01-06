84ef03a066726a48a8f0bcc8108955590caa1b25
Bryan Goldstein <brysgo@gmail.com>
2012-01-06 10:16:28 -0500

    added skeleton tests cases.

--- database_test.py
+++ database_test.py
@@ -23,14 +23,16 @@ from google.appengine.ext import testbed
 from google.appengine.datastore import datastore_stub_util
 from database import *
 
-class ExperimentOneTests(unittest.TestCase):
-  
+class DefaultTestClass(unittest.TestCase):
+  '''
+  Setup, teardown, and assorted utility functions for all test cases.
+  '''
   def setUp(self):
     # Create test bed
     self.testbed = testbed.Testbed()
     self.testbed.activate()
     # Create a consistency policy that will simulate the High Replication consistency model.
-    self.policy = datastore_stub_util.PseudoRandomHRConsistencyPolicy(probability=1)
+    self.policy = datastore_stub_util.PseudoRandomHRConsistencyPolicy(probability=0.7)
     # Initialize the datastore stub with this policy.
     self.testbed.init_datastore_v3_stub(consistency_policy=self.policy)
     self.testbed.init_memcache_stub()
@@ -38,3 +40,124 @@ class ExperimentOneTests(unittest.TestCase):
     
   def teardown(self):
     self.testbed.deactivate()
+
+class BuilderQuestionTests(DefaultTestClass):
+  '''
+  Make sure builder questions are functioning properly.
+  '''
+  
+  def testAssignBuilderQuestion(self):
+    '''
+    Tests that builder questions get assigned properly.
+    '''
+    pass
+    
+  def testAnswerBuilderQuestion(self):
+    '''
+    Tests that a builder question creates a new question upon answering.
+    '''
+    pass
+    
+  def testSerializeBuilderQuestion(self):
+    '''
+    Tests that the builder question gets serialized properly.
+    '''
+    pass
+
+class ShortAnswerQuestionTests(DefaultTestClass):
+  '''
+  Make sure short answer questions are functioning properly.
+  '''
+  
+  def testAssignShortAnswerQuestion(self):
+    '''
+    Tests that short answer questions get assigned properly.
+    '''
+    pass
+    
+  def testAnswerShortAnswerQuestion(self):
+    '''
+    Tests that short answer questions behave as expected when answered.
+    '''
+    pass
+    
+  def testSerializeShortAnswerQuestion(self):
+    '''
+    Tests that a short answer question gets serialized properly.
+    '''
+    pass
+    
+class GraderQuestionTests(DefaultTestClass):
+  '''
+  Make sure grader questions are functioning as they should.
+  '''
+  
+  def testAssignGraderQuestion(self):
+    '''
+    Tests that the grader question gets assigned properly.
+    '''
+    pass
+    
+  def testAnswerGraderQuestion(self):
+    '''
+    Tests that the grading works.
+    '''
+    pass
+  
+  def testSerializeGraderQuestion(self):
+    '''
+    Tests that grader questions get serialized properly.
+    '''
+    pass
+
+class ConfidentGraderQuestionTests(DefaultTestClass):
+  '''
+  Make sure grader questions are functioning as they should.
+  '''
+
+  def testAssignConfidentGraderQuestion(self):
+    '''
+    Tests that the grader question gets assigned properly.
+    '''
+    pass
+
+  def testAnswerConfidentGraderQuestion(self):
+    '''
+    Tests that the grading works.
+    '''
+    pass
+
+  def testSerializeConfidentGraderQuestion(self):
+    '''
+    Tests that grader questions get serialized properly.
+    '''
+    pass
+
+class FollowupQuestionTests(DefaultTestClass):
+  '''
+  Make sure everything related to followup questions is working.
+  '''
+    
+  def testAssignFollowupBuilderQuestion(self):
+    '''
+    Tests assigning a followup builder question.
+    '''
+    pass
+    
+  def testAnswerFollowupBuilderQuestion(self):
+    '''
+    Tests creation of a followup question.
+    '''
+    pass
+    
+  def testAssignFollowupQuestion(self):
+    '''
+    Tests that followup question gets assigned when it should.
+    '''
+    pass
+  
+  def testSerializeFollowupQuestion(self):
+    '''
+    Tests that a followup question gets serialized properly.
+    '''
+    pass
