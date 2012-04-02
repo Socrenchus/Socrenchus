2b1e3abb97de39a67f8ac3dc0b550341631394dd
Prayrit Jain <prashn64@ubuntu.(none)>
2012-04-02 16:08:59 -0400

    added built files to the repo

---
+++ client/main.js
@@ -0,0 +1,158 @@
+(function() {
+  var __hasProp = Object.prototype.hasOwnProperty,
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor; child.__super__ = parent.prototype; return child; };
+
+  $(function() {
+    /*
+      # Core Model and Logic
+    */
+    var App, Post, PostView, Posts, StreamView, Templates, Workspace, app_router, e, postCollection, _i, _len, _ref;
+    Post = (function(_super) {
+
+      __extends(Post, _super);
+
+      function Post() {
+        Post.__super__.constructor.apply(this, arguments);
+      }
+
+      Post.prototype.respond = function(response) {
+        var p;
+        p = new Post({
+          value: response
+        });
+        return this.save({
+          responses: this.get('responses').push(p.cid)
+        });
+      };
+
+      return Post;
+
+    })(Backbone.Model);
+    Posts = (function(_super) {
+
+      __extends(Posts, _super);
+
+      function Posts() {
+        Posts.__super__.constructor.apply(this, arguments);
+      }
+
+      Posts.prototype.model = Post;
+
+      Posts.prototype.localStorage = new Store('posts');
+
+      return Posts;
+
+    })(Backbone.Collection);
+    /*
+      # View
+    */
+    Templates = {};
+    _ref = $('#templates').children();
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      e = _ref[_i];
+      Templates[e.id] = $(e).html();
+    }
+    PostView = (function(_super) {
+
+      __extends(PostView, _super);
+
+      function PostView() {
+        PostView.__super__.constructor.apply(this, arguments);
+      }
+
+      PostView.prototype.tagName = 'li';
+
+      PostView.prototype.className = 'post';
+
+      PostView.prototype.template = Templates.post;
+
+      PostView.prototype.initialize = function() {
+        return this.id = this.model.id;
+      };
+
+      PostView.prototype.render = function() {
+        $(this.el).html(this.template);
+        $(this.el).find('#answer').omnipost();
+        return $(this.el);
+      };
+
+      return PostView;
+
+    })(Backbone.View);
+    StreamView = (function(_super) {
+
+      __extends(StreamView, _super);
+
+      function StreamView() {
+        StreamView.__super__.constructor.apply(this, arguments);
+      }
+
+      StreamView.prototype.initialize = function() {
+        postCollection.bind('add', this.addOne, this);
+        postCollection.bind('reset', this.addAll, this);
+        return postCollection.fetch();
+      };
+
+      StreamView.prototype.addOne = function(item) {
+        var post;
+        post = new PostView({
+          model: item
+        });
+        return $(post.render()).appendTo();
+      };
+
+      StreamView.prototype.addAll = function() {
+        return postCollection.each(this.addOne);
+      };
+
+      return StreamView;
+
+    })(Backbone.View);
+    postCollection = new Posts();
+    App = new StreamView({
+      el: $('#learn')
+    });
+    /*
+      # Routes
+    */
+    Workspace = (function(_super) {
+
+      __extends(Workspace, _super);
+
+      function Workspace() {
+        Workspace.__super__.constructor.apply(this, arguments);
+      }
+
+      Workspace.prototype.routes = {
+        '/:id': 'assign',
+        'mockup': 'mockup'
+      };
+
+      Workspace.prototype.assign = function(id) {
+        postCollection.get(id);
+        return postCollection.fetch();
+      };
+
+      Workspace.prototype.mockup = function() {
+        var p, pv;
+        p = new Post({
+          content: 'This is an example post.',
+          topic_tags: '',
+          rubric_tags: '',
+          parents: '',
+          responses: ''
+        });
+        pv = new PostView({
+          model: p
+        });
+        return $('#assignments').append(pv.render());
+      };
+
+      return Workspace;
+
+    })(Backbone.Router);
+    app_router = new Workspace();
+    return Backbone.history.start();
+  });
+
+}).call(this);
---
+++ client/static/lib/jquery.ui.votebox.js
@@ -0,0 +1,110 @@
+(function() {
+  ;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+
+  (function($, window, document) {
+    var Plugin, defaults, pluginName;
+    pluginName = 'votebox';
+    defaults = {
+      votesnum: 0
+    };
+    Plugin = (function() {
+
+      function Plugin(element, options) {
+        this.element = element;
+        this.voteCount = __bind(this.voteCount, this);
+        this.options = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = 'votebox';
+        this.init();
+      }
+
+      Plugin.prototype.init = function() {
+        var currentArrowImage, downArrow, downpressed, originalvotesnum, upArrow, uppressed, voteboxdiv, votetext,
+          _this = this;
+        uppressed = false;
+        downpressed = false;
+        originalvotesnum = this.options.votesnum;
+        currentArrowImage = "/images/votearrow.png";
+        voteboxdiv = $("<div class = 'ui-votebox'></div>");
+        upArrow = $("<img alt='^' title='vote up' id='ui-upvote'>");
+        upArrow.attr('src', '/images/votearrow.png');
+        upArrow.attr('onmouseover', 'src="/images/votearrowover.png"');
+        upArrow.attr('onmousedown', 'src="/images/votearrowdown.png"');
+        upArrow.attr('onmouseout', 'src="/images/votearrow.png"');
+        upArrow.attr('onmouseup', 'src="/images/votearrow.png"');
+        votetext = $("<div id='ui-votetext'>" + originalvotesnum + "</div>");
+        downArrow = $("<img alt='v' title='vote down' id='ui-downvote'>");
+        downArrow.attr('src', '/images/votearrow.png');
+        downArrow.attr('onmouseover', 'src="/images/votearrowover.png"');
+        downArrow.attr('onmousedown', 'src="/images/votearrowdown.png"');
+        downArrow.attr('onmouseout', 'src="/images/votearrow.png"');
+        downArrow.attr('onmouseup', 'src="/images/votearrow.png"');
+        voteboxdiv.append(upArrow);
+        voteboxdiv.append(votetext);
+        voteboxdiv.append(downArrow);
+        $(this.element).append(voteboxdiv);
+        upArrow.click(function() {
+          if (uppressed === false) {
+            _this.voteCount(originalvotesnum + 1);
+            upArrow.attr('src', '/images/votearrowcomplete.png');
+            upArrow.attr('onmouseout', 'src="/images/votearrowcomplete.png"');
+            upArrow.attr('onmouseup', 'src="/images/votearrowcomplete.png"');
+            uppressed = true;
+          } else {
+            _this.voteCount(originalvotesnum);
+            upArrow.attr('src', '/images/votearrow.png');
+            upArrow.attr('onmouseout', 'src="/images/votearrow.png"');
+            upArrow.attr('onmouseup', 'src="/images/votearrow.png"');
+            uppressed = false;
+          }
+          votetext.text(_this.voteCount());
+          downArrow.attr('src', '/images/votearrow.png');
+          downArrow.attr('onmouseout', 'src="/images/votearrow.png"');
+          downArrow.attr('onmouseup', 'src="/images/votearrow.png"');
+          return downpressed = false;
+        });
+        return downArrow.click(function() {
+          if (downpressed === false) {
+            _this.voteCount(originalvotesnum - 1);
+            downArrow.attr('src', '/images/votearrowcomplete.png');
+            downArrow.attr('onmouseout', 'src="/images/votearrowcomplete.png"');
+            downArrow.attr('onmouseup', 'src="/images/votearrowcomplete.png"');
+            downpressed = true;
+          } else {
+            _this.voteCount(originalvotesnum);
+            downArrow.attr('src', '/images/votearrow.png');
+            downArrow.attr('onmouseout', 'src="/images/votearrow.png"');
+            downArrow.attr('onmouseup', 'src="/images/votearrow.png"');
+            downpressed = false;
+          }
+          votetext.text(_this.voteCount());
+          upArrow.attr('src', '/images/votearrow.png');
+          upArrow.attr('onmouseout', 'src="/images/votearrow.png"');
+          upArrow.attr('onmouseup', 'src="/images/votearrow.png"');
+          return uppressed = false;
+        });
+      };
+
+      Plugin.prototype.voteCount = function(newVotesNum) {
+        if (newVotesNum == null) newVotesNum = null;
+        if (newVotesNum === null) {
+          return this._defaults.votesnum;
+        } else {
+          return this._defaults.votesnum = newVotesNum;
+        }
+      };
+
+      return Plugin;
+
+    })();
+    return $.fn[pluginName] = function(options) {
+      return this.each(function() {
+        if (!$.data(this, "plugin_" + pluginName)) {
+          return $.data(this, "plugin_" + pluginName, new Plugin(this, options));
+        }
+      });
+    };
+  })(jQuery, window, document);
+
+}).call(this);
