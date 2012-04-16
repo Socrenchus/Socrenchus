73c14497e6e8485572a921d3574f02fd707a2b6f
Bryan Goldstein <brysgo@gmail.com>
2012-04-16 17:49:10 -0400

    default to root post as parent.

--- server/database.py
+++ server/database.py
@@ -318,6 +318,8 @@ class Stream(ndb.Model):
     """
     Creates a post from given content with optional parent.
     """
+    if not parent:
+      parent = Post.get_root()
     p = Post(parent=parent,content=content)
     p.put()
     Tag(title=',assignment', user=self.user, parent=p).put()
