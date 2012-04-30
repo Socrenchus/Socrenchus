cb35c4aaab2e48cd5f131bef550d8178b91fd10e
Prayrit Jain <prashn64@ubuntu.(none)>
2012-04-30 16:25:04 -0400

    fixed a problem with assignment bootstrapping where the key of a child post wouldnt match up with the passed in id

--- server/boot_strap.py
+++ server/boot_strap.py
@@ -20,18 +20,22 @@ class BootStrap:
       return key
     queryMap = Post.query(Post.key==key).map(post_enum)
     if queryMap.count(None) == 0:
+      postkeymap = {}
       filereader = open(filename)
       filecontents = filereader.read()
       fullcontents = json.simplejson.loads(filecontents)
       posts = fullcontents[0]
       for i in range(len(posts)):
         user = posts[i]['user']
+        fullid = filename+str(posts[i]['id'])
         stream = BootStrap.switchToUser(user)
+        ndbpost = None
         if posts[i]['parent'] != "None":
-          parentkey = ndb.Key(Post, filename + posts[i]['parent'])
-          ndbpost = Post.get_or_insert(filename+str(posts[i]['id']), parent=parentkey, content=posts[i]['content'])
+          parentkey = postkeymap[filename + posts[i]['parent']]
+          ndbpost = Post.get_or_insert(fullid, parent=parentkey, content=posts[i]['content'])
         else:
-          ndbpost = Post.get_or_insert(filename+str(posts[i]['id']), content=posts[i]['content'])
+          ndbpost = Post.get_or_insert(fullid, content=posts[i]['content'])
+        postkeymap[fullid] = ndbpost.key        
         stream.assign_post(ndb.Key(urlsafe=ndbpost.key.urlsafe()))
 
       tags = fullcontents[1]
@@ -39,22 +43,6 @@ class BootStrap:
       for i in range(len(tags)):
         user = tags[i]['user']
         stream = BootStrap.switchToUser(user)
-        
-        parentkey = ndb.Key(Post, filename + tags[i]['parent'])
+        parentkey = postkeymap[filename + tags[i]['parent']]
         Tag.get_or_create(tags[i]['title'],parentkey)
 
-  """
-  @classmethod
-  def loadtags(self,filename):
-    keyname = filename + "0"
-    key = ndb.Key(Tag, keyname)
-    def post_enum(key):
-      return key
-    queryMap = Post.query(Tag.key==key).map(post_enum)
-    if queryMap.count(None) == 0:
-      filereader = open(filename)
-      filecontents = filereader.read()
-      tags = json.simplejson.loads(filecontents)
-      for i in range(len(tags)):
-  """      
-      
